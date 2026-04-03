780c43d24e81ff2c752236f5c3bc37335f927158
nishant7t <oknlogintnissan@gmail.com>
2026-04-03 11:43:00 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { login as loginApi } from '../utils/api';
-import { useAuth } from '../context/AuthContext';
+import { login as loginApi } from './api';
+import { useAuth } from './AuthContext';
 import './Auth.css';
 
 export default function Login() {
