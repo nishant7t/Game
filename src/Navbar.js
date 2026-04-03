0fc7800b3627d43ddcb969bba6397b7243f33711
nishant7t <oknlogintnissan@gmail.com>
2026-04-03 11:57:05 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from './AuthContext';
 import './Navbar.css';
 
 export default function Navbar() {
