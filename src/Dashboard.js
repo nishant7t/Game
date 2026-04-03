5fde0faf7df78fb42189488cbd4b8c3b8d87cd76
nishant7t <oknlogintnissan@gmail.com>
2026-04-03 12:11:41 +0530

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from './AuthContext';
 import './Dashboard.css';
 
 const GAMES = [
