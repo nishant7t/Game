f2fde82295a048447eff65302870c7c2741e9497
nishant7t <oknlogintnissan@gmail.com>
2026-04-03 12:06:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,8 +65,9 @@ mongoose
   .then(() => {
     console.log('✅ MongoDB connected');
     const PORT = process.env.PORT || 10000;
-    app.listen(PORT,'0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
-  })
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+})
   .catch((err) => {
     console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
